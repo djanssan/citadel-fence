b46b3d29c6642f1eb118b602e595dca45fd5c24d
djanssan <peter.motin@mail.com>
2023-08-14 19:05:40 +0300

    finalizing styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88d44173d7278cca"; }
+/******/ 		__webpack_require__.h = function() { return "6dd4c33fb788dab3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
